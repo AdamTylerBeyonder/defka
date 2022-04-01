757e782deb7116d7de201e8c505e33c1cc5be780
Adminokoji <sparrow0golden1beyonder@gmail.com>
2022-04-01 12:53:28 +0600

    project1

---
+++ test.py
@@ -0,0 +1,16 @@
+def apply_twice(func, arg):
+    return func(func(arg))
+
+def add_five(x):
+    return x + 5
+
+print(apply_twice(add_five, 10))
+
+def pure_function(x, y):
+    temp = x + 2*y
+    return temp / (2*x + y)
+
+some_li = []
+
+def impure(arg):
+    some_li.append(arg)
